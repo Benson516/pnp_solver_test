Fix the transformation between Euler angles and rotation matrix (correct the yaw angle); modify the reprojection

scripts/PNP_SOLVER_LIB.py
@@ -18,14 +18,9 @@ class PNP_SOLVER_A2_M3(object):
         # Convert to numpy vector, shape: (3,1)
         # Applying the scale as well
         self.np_point_3d_dict = dict()
-        self.lib_print("-"*35)
-        self.lib_print("3D points in local coordinate:")
-        self.lib_print("pattern_scale = %f" % pattern_scale)
         for _k in self.point_3d_dict:
             self.np_point_3d_dict[_k] = np.array(self.point_3d_dict[_k]).reshape((3,1))
             self.np_point_3d_dict[_k] *= pattern_scale # Multiply the scale
-            self.lib_print("%s:\n%s" % (_k, str(self.np_point_3d_dict[_k])))
-        self.lib_print("-"*35)
         # self.lib_print(self.np_point_3d_dict)
 
         # Backup
@@ -35,14 +30,36 @@ class PNP_SOLVER_A2_M3(object):
         #---------------------------#
         self.is_using_pre_transform = False
         # self.is_using_pre_transform = True
-        # self.pre_trans_R_a_h = np.eye(3)
-        # self.pre_trans_t_a_h = np.array([[0.0, 0.0, 1.0]]).T
-        self.pre_trans_R_a_h = self.get_rotation_matrix_from_Euler(0.0, 0.0, 45.0, is_degree=True)
-        self.pre_trans_t_a_h = np.array([[0.0, 0.0, 0.0]]).T
+        self.pre_trans_R_a_h = np.eye(3)
+        self.pre_trans_t_a_h = np.array([[0.0, 0.0, 1.0]]).T
+        # self.pre_trans_R_a_h = self.get_rotation_matrix_from_Euler(0.0, 0.0, 45.0, is_degree=True)
+        # self.pre_trans_t_a_h = np.array([[0.0, 0.0, 0.0]]).T
         if self.is_using_pre_transform:
             for _k in self.np_point_3d_pretransfer_dict:
                 self.np_point_3d_pretransfer_dict[_k] = self.transform_3D_point(self.np_point_3d_pretransfer_dict[_k], self.pre_trans_R_a_h, self.pre_trans_t_a_h)
+        # For storing the estimated result of R_ca and t_ca
+        self.np_R_c_a_est = np.eye(3)
+        self.np_t_c_a_est = np.zeros((3,1))
         #---------------------------#
+
+
+        self.lib_print("-"*35)
+        self.lib_print("3D points in local coordinate:")
+        self.lib_print("pattern_scale = %f" % pattern_scale)
+        for _k in self.point_3d_dict:
+            # self.lib_print("%s:\n%s" % (_k, str(self.np_point_3d_dict[_k])))
+            # self.lib_print("%s:\n%s" % (_k, str(self.np_point_3d_pretransfer_dict[_k])))
+            np.set_printoptions(suppress=True, precision=2)
+            print("%s:%sp_3D=%s.T | p_3D_pretrans=%s.T" %
+                (   _k,
+                    " "*(12-len(_k)),
+                    str(self.np_point_3d_dict[_k].T),
+                    str(self.np_point_3d_pretransfer_dict[_k].T)
+                )
+            )
+            np.set_printoptions(suppress=False, precision=8)
+        self.lib_print("-"*35)
+        #
 
         self.verbose = verbose
 
@@ -165,13 +182,15 @@ class PNP_SOLVER_A2_M3(object):
 
         # test, pre-transfer
         #---------------------------#
+        self.np_R_c_a_est = copy.deepcopy(np_R_est)
+        self.np_t_c_a_est = copy.deepcopy(np_t_est)
         if self.is_using_pre_transform:
             np_R_c_h_est = np_R_est @ self.pre_trans_R_a_h # R_ch = R_ca @ R_ah
             np_t_c_h = np_R_est @ self.pre_trans_t_a_h + np_t_est # t_ch = R_ca @ t_ah + t_ca
             # Overwrite output
-            np_R_est = np_R_c_h_est
-            np_t_est = np_t_c_h
-            t3_est = np_t_c_h[2,0]
+            np_R_est = copy.deepcopy(np_R_c_h_est)
+            np_t_est = copy.deepcopy(np_t_c_h)
+            t3_est = np_t_est[2,0]
         #---------------------------#
 
         # Convert to Euler angle
@@ -419,6 +438,10 @@ class PNP_SOLVER_A2_M3(object):
 
         is_degree - True: the angle unit is degree, False: the angle unit is rad
         '''
+        # Mirror correction term
+        #------------------------------#
+        yaw = -yaw
+        #------------------------------#
         if is_degree:
             roll = np.deg2rad(roll)
             yaw = np.deg2rad(yaw)
@@ -473,7 +496,10 @@ class PNP_SOLVER_A2_M3(object):
             roll = np.rad2deg(roll)
             yaw = np.rad2deg(yaw)
             pitch = np.rad2deg(pitch)
-        #
+        # Mirror correction term
+        #------------------------------#
+        yaw = -yaw
+        #------------------------------#
         return (roll, yaw, pitch)
     #-----------------------------------------------------------#
 
@@ -542,7 +568,7 @@ class PNP_SOLVER_A2_M3(object):
         return (uv_o, dir_x, dir_y, dir_z)
 
 
-    def perspective_projection_golden_landmarks(self, np_R, np_t, is_quantized=False):
+    def perspective_projection_golden_landmarks(self, np_R, np_t, is_quantized=False, is_pretrans_points=False):
         '''
         Project the golden landmarks onto the image space using the given camera intrinsic.
         '''
@@ -555,7 +581,11 @@ class PNP_SOLVER_A2_M3(object):
         #--------------------------------------------------#
         # Not the self.np_point_3d_pretransfer_dict
         for _k in self.np_point_3d_dict:
-            _np_point_image, _projection_no_q = self.perspective_projection(self.np_point_3d_dict[_k], self.np_K_camera_est, np_R, np_t, is_quantized=is_quantized)
+            if is_pretrans_points:
+                _point = self.np_point_3d_pretransfer_dict[_k]
+            else:
+                _point = self.np_point_3d_dict[_k]
+            _np_point_image, _projection_no_q = self.perspective_projection(_point, self.np_K_camera_est, np_R, np_t, is_quantized=is_quantized)
             np_point_image_dict[_k] = _np_point_image
             np_point_quantization_error_dict[_k] = (_np_point_image - _projection_no_q)
             np_point_image_no_q_err_dict[_k] = _projection_no_q
@@ -673,9 +703,12 @@ def main():
     pnp_solver = PNP_SOLVER_A2_M3(np_K_camera_est, point_3d_dict, verbose=True)
 
     # test
-    roll = 10.0
-    yaw = 35.0
-    pitch = -25.0
+    # roll = 10.0
+    # yaw = 35.0
+    # pitch = -25.0
+    roll = 45.0
+    yaw = 40.0
+    pitch = -15.0
     print("(roll, yaw, pitch) = %s" % str((roll, yaw, pitch)))
     R_1 = pnp_solver.get_rotation_matrix_from_Euler( roll, yaw, pitch, is_degree=True)
     roll_1, yaw_1, pitch_1 = pnp_solver.get_Euler_from_rotation_matrix(R_1, is_degree=True)

scripts/huey_data_test_cv.py
@@ -122,7 +122,7 @@ point_3d_dict["eye_l_96"] = [ 0.032, 0.0, 0.0] # [ 0.035, 0.0, 0.0]
 point_3d_dict["eye_r_97"] = [-0.032, 0.0, 0.0] # [ 0.035, 0.0, 0.0]
 point_3d_dict["mouse_l_76"] = [ 0.027, 0.070, 0.0] # [ 0.025, 0.085, 0.0]
 point_3d_dict["mouse_r_82"] = [ -0.027, 0.070, 0.0] # [ -0.025, 0.085, 0.0]
-point_3d_dict["nose_t_54"] = [ 0.0, 0.0455, 0.03] # [ 0.0, 0.046, 0.03]
+point_3d_dict["nose_t_54"] = [ -0.005, 0.0455, 0.03] # [ 0.0, 0.0455, 0.03] # [ 0.0, 0.046, 0.03]
 point_3d_dict["eye_c_51"] = [0.0, 0.0, 0.0]
 point_3d_dict["chin_t_16"] = [0.0, 0.12, 0.0]
 # point_3d_dict["face_c"] = [ 0.0, 0.035, 0.0]
@@ -193,6 +193,10 @@ for _idx in range(len(data_list)):
 
     # Solve
     np_R_est, np_t_est, t3_est, roll_est, yaw_est, pitch_est, res_norm = pnp_solver.solve_pnp(np_point_image_dict)
+    np_R_ca_est = pnp_solver.np_R_c_a_est
+    np_t_ca_est = pnp_solver.np_t_c_a_est
+    # np_R_ca_est = copy.deepcopy(pnp_solver.np_R_c_a_est)
+    # np_t_ca_est = copy.deepcopy(pnp_solver.np_t_c_a_est)
 
     # Compare result
     #-----------------------------#
@@ -208,8 +212,9 @@ for _idx in range(len(data_list)):
 
 
     # Reprojections
-    np_point_image_dict_reproject = pnp_solver.perspective_projection_golden_landmarks(np_R_est, np_t_est, is_quantized=False)
-    np_point_image_dict_reproject_GT_ori_golden_patern = pnp_solver.perspective_projection_golden_landmarks(np_R_GT, np_t_GT_est, is_quantized=False)
+    # np_point_image_dict_reproject = pnp_solver.perspective_projection_golden_landmarks(np_R_est, np_t_est, is_quantized=False, is_pretrans_points=False)
+    np_point_image_dict_reproject = pnp_solver.perspective_projection_golden_landmarks(np_R_ca_est, np_t_ca_est, is_quantized=False, is_pretrans_points=True)
+    np_point_image_dict_reproject_GT_ori_golden_patern = pnp_solver.perspective_projection_golden_landmarks(np_R_GT, np_t_GT_est, is_quantized=False, is_pretrans_points=False)
     #
     print("2D points on image (re-projection):")
     # print("2D points on image (is_h_mirrored_image=%s):" % str(is_h_mirrored_image))
@@ -221,7 +226,7 @@ for _idx in range(len(data_list)):
                 " "*(12-len(_k)),
                 str(np_point_image_dict[_k].T),
                 str(np_point_image_dict_reproject[_k].T),
-                str((np_point_image_dict_reproject[_k]-np_point_image_dict[_k]).T),
+                str((np_point_image_dict_reproject[_k]-np_point_image_dict[_k]).T)
             )
         )
         np.set_printoptions(suppress=False, precision=8)
@@ -234,7 +239,7 @@ for _idx in range(len(data_list)):
     print("np_R_est = \n%s" % str(np_R_est))
     _det = np.linalg.det(np_R_est)
     print("_det = %f" % _det)
-    print("(roll_est, yaw_est, pitch_est) \t\t= %s" % str( np.rad2deg( (roll_est, yaw_est, pitch_est) ) ) )
+    # print("(roll_est, yaw_est, pitch_est) \t\t= %s" % str( np.rad2deg( (roll_est, yaw_est, pitch_est) ) ) )
     print("t3_est = %f" % t3_est)
     print("np_t_est = \n%s" % str(np_t_est))
     print()