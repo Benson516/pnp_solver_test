76bb4e92ddf8cda50613ed3e618fd6505284db49
benson516 <benson516@hotmail.com>
2021-06-11 15:47:47 +0800

    add HMI format to the random stress test

--- scripts/random_stress_test.py
+++ scripts/random_stress_test.py
@@ -35,8 +35,11 @@ is_stress_test = False
 # is_random = True
 is_random = False
 #
-# is_quantized = True
-is_quantized = False
+is_quantized = True
+# is_quantized = False
+
+# stop_at_fail_cases = True
+stop_at_fail_cases = False
 
 #
 # DATA_COUNT = 3
@@ -185,31 +188,66 @@ pnp_solver_GT = PNPS.PNP_SOLVER_A2_M3(np_K_camera_GT, point_3d_dict_list, patter
 # Ground truth classification
 #-------------------------------#
 
-# class label and bins
-# Depth
-class_depth_nominal_value = np.arange(20.0, 240.1, 20) # Note: the length of label should be one element longer than the bin
-class_depth_label = [str(int(_e)) for _e in class_depth_nominal_value] # Using nominal value as class label
-class_depth_bins = list( class_depth_nominal_value[:-1] + 10.0 ) # Note: Keep the last label. Calculate the upper bound, since the np.digitize() return the index of ubber bound bin
-print("class_depth_label = %s" % class_depth_label)
-print("class_depth_bins = %s" % class_depth_bins)
-# Roll
-class_roll_nominal_value = np.array([-45, -25, 0, 25, 45]) # Note: the length of label should be one element longer than the bin
-class_roll_label = [str(int(_e)) for _e in class_roll_nominal_value] # Using nominal value as class label
-class_roll_bins = [-35, -15, 15, 35] # Only the middle bound values
-print("class_roll_label = %s" % class_roll_label)
-print("class_roll_bins = %s" % class_roll_bins)
-# Pitch
-class_pitch_nominal_value = np.array([-30, -15, 0, 15, 30]) # Note: the length of label should be one element longer than the bin
-class_pitch_label = [str(int(_e)) for _e in class_pitch_nominal_value] # Using nominal value as class label
-class_pitch_bins = [-23, -8, 8, 23] # Only the middle bound values
-print("class_pitch_label = %s" % class_pitch_label)
-print("class_pitch_bins = %s" % class_pitch_bins)
-# Yaw
-class_yaw_nominal_value = np.array([-40, -20, 0, 20, 40]) # Note: the length of label should be one element longer than the bin
-class_yaw_label = [str(int(_e)) for _e in class_yaw_nominal_value] # Using nominal value as class label
-class_yaw_bins = [-30, -10, 10, 30] # Only the middle bound values
-print("class_yaw_label = %s" % class_yaw_label)
-print("class_yaw_bins = %s" % class_yaw_bins)
+# Formate
+# drpy_class_format = "drpy_expand"
+drpy_class_format = "HMI_inspection"
+
+if drpy_class_format == "drpy_expand":
+    # class label and bins
+    # Depth
+    class_depth_nominal_value = np.arange(20.0, 240.1, 20) # Note: the length of label should be one element longer than the bin
+    class_depth_label = [str(int(_e)) for _e in class_depth_nominal_value] # Using nominal value as class label
+    class_depth_bins = list( class_depth_nominal_value[:-1] + 10.0 ) # Note: Keep the last label. Calculate the upper bound, since the np.digitize() return the index of ubber bound bin
+    print("class_depth_label = %s" % class_depth_label)
+    print("class_depth_bins = %s" % class_depth_bins)
+    # Roll
+    class_roll_nominal_value = np.array([-45, -25, 0, 25, 45]) # Note: the length of label should be one element longer than the bin
+    class_roll_label = [str(int(_e)) for _e in class_roll_nominal_value] # Using nominal value as class label
+    class_roll_bins = [-35, -15, 15, 35] # Only the middle bound values
+    print("class_roll_label = %s" % class_roll_label)
+    print("class_roll_bins = %s" % class_roll_bins)
+    # Pitch
+    class_pitch_nominal_value = np.array([-30, -15, 0, 15, 30]) # Note: the length of label should be one element longer than the bin
+    class_pitch_label = [str(int(_e)) for _e in class_pitch_nominal_value] # Using nominal value as class label
+    class_pitch_bins = [-23, -8, 8, 23] # Only the middle bound values
+    print("class_pitch_label = %s" % class_pitch_label)
+    print("class_pitch_bins = %s" % class_pitch_bins)
+    # Yaw
+    class_yaw_nominal_value = np.array([-40, -20, 0, 20, 40]) # Note: the length of label should be one element longer than the bin
+    class_yaw_label = [str(int(_e)) for _e in class_yaw_nominal_value] # Using nominal value as class label
+    class_yaw_bins = [-30, -10, 10, 30] # Only the middle bound values
+    print("class_yaw_label = %s" % class_yaw_label)
+    print("class_yaw_bins = %s" % class_yaw_bins)
+    #
+elif drpy_class_format == "HMI_inspection":
+    # class label and bins
+    # Depth
+    class_depth_label = ["0", "100", "200"] # Using lower bound as the label
+    class_depth_bins = [100, 200] # Only the middle bound values
+    print("class_depth_label = %s" % class_depth_label)
+    print("class_depth_bins = %s" % class_depth_bins)
+    # Roll
+    class_roll_nominal_value = np.array([0]) # Note: the length of label should be one element longer than the bin
+    class_roll_label = [str(int(_e)) for _e in class_roll_nominal_value] # Using nominal value as class label
+    class_roll_bins = [] # Only the middle bound values
+    print("class_roll_label = %s" % class_roll_label)
+    print("class_roll_bins = %s" % class_roll_bins)
+    # Pitch
+    class_pitch_nominal_value = np.array([-30, -15, 0, 15, 30]) # Note: the length of label should be one element longer than the bin
+    class_pitch_label = [str(int(_e)) for _e in class_pitch_nominal_value] # Using nominal value as class label
+    class_pitch_bins = [-23, -8, 8, 23] # Only the middle bound values
+    print("class_pitch_label = %s" % class_pitch_label)
+    print("class_pitch_bins = %s" % class_pitch_bins)
+    # Yaw
+    class_yaw_nominal_value = np.array([-40, -20, 0, 20, 40]) # Note: the length of label should be one element longer than the bin
+    class_yaw_label = [str(int(_e)) for _e in class_yaw_nominal_value] # Using nominal value as class label
+    class_yaw_bins = [-30, -10, 10, 30] # Only the middle bound values
+    print("class_yaw_label = %s" % class_yaw_label)
+    print("class_yaw_bins = %s" % class_yaw_bins)
+    #
+else:
+    pass
+#-------------------------------#
 #-------------------------------#
 
 # Convert the original data to structured data_list
@@ -587,10 +625,11 @@ for _idx in range(len(data_list)):
 
     # Break the stress test
     #----------------------------#
-    if is_stress_test:
-        if pass_count < pass_count_treshold:
-            print("Fail, break the stress test!!")
-            is_continuing_to_next_sample = False
+    if stop_at_fail_cases:
+        if is_stress_test:
+            if pass_count < pass_count_treshold:
+                print("Fail, break the stress test!!")
+                is_continuing_to_next_sample = False
     #----------------------------#
 
 
