1d6db5135e055f3db74219623f06e1e243fa596e
benson516 <benson516@hotmail.com>
2021-07-19 17:05:13 +0800

    Changed to use the classification in toolbox; added the automatic creation of directories

--- scripts/random_stress_test.py
+++ scripts/random_stress_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import copy
 import time
 import csv
+import os
 #
 import cv2
 #
@@ -29,6 +30,13 @@ image_result_dir_str = '/home/benson516/test_PnP_solver/dataset/Huey_face_landma
 result_csv_dir_str = '/home/benson516/test_PnP_solver/dataset/Huey_face_landmarks_pose/result_CSVs/'
 result_csv_file_prefix_str = "result_csv_"
 result_statistic_txt_file_prefix_str = "statistic_"
+
+# Make directories for storing the results if they do not exist.
+#------------------------------------------------------------------#
+os.makedirs(image_result_unflipped_dir_str, mode=0o777, exist_ok=True)
+os.makedirs(image_result_dir_str, mode=0o777, exist_ok=True)
+os.makedirs(result_csv_dir_str, mode=0o777, exist_ok=True)
+#------------------------------------------------------------------#
 
 # Behavior of this program
 #---------------------------#
@@ -223,61 +231,8 @@ pnp_solver = PNPS.PNP_SOLVER(np_K_camera_est, point_3d_dict_list, pattern_scale_
 drpy_class_format = "drpy_expand"
 # drpy_class_format = "HMI_inspection"
 
-if drpy_class_format == "drpy_expand":
-    # class label and bins
-    # Depth
-    class_depth_nominal_value = np.arange(20.0, 240.1, 20) # Note: the length of label should be one element longer than the bin
-    class_depth_label = [str(int(_e)) for _e in class_depth_nominal_value] # Using nominal value as class label
-    class_depth_bins = list( class_depth_nominal_value[:-1] + 10.0 ) # Note: Keep the last label. Calculate the upper bound, since the np.digitize() return the index of ubber bound bin
-    print("class_depth_label = %s" % class_depth_label)
-    print("class_depth_bins = %s" % class_depth_bins)
-    # Roll
-    class_roll_nominal_value = np.array([-45, -25, 0, 25, 45]) # Note: the length of label should be one element longer than the bin
-    class_roll_label = [str(int(_e)) for _e in class_roll_nominal_value] # Using nominal value as class label
-    class_roll_bins = [-35, -15, 15, 35] # Only the middle bound values
-    print("class_roll_label = %s" % class_roll_label)
-    print("class_roll_bins = %s" % class_roll_bins)
-    # Pitch
-    class_pitch_nominal_value = np.array([-30, -15, 0, 15, 30]) # Note: the length of label should be one element longer than the bin
-    class_pitch_label = [str(int(_e)) for _e in class_pitch_nominal_value] # Using nominal value as class label
-    class_pitch_bins = [-23, -8, 8, 23] # Only the middle bound values
-    print("class_pitch_label = %s" % class_pitch_label)
-    print("class_pitch_bins = %s" % class_pitch_bins)
-    # Yaw
-    class_yaw_nominal_value = np.array([-40, -20, 0, 20, 40]) # Note: the length of label should be one element longer than the bin
-    class_yaw_label = [str(int(_e)) for _e in class_yaw_nominal_value] # Using nominal value as class label
-    class_yaw_bins = [-30, -10, 10, 30] # Only the middle bound values
-    print("class_yaw_label = %s" % class_yaw_label)
-    print("class_yaw_bins = %s" % class_yaw_bins)
-    #
-elif drpy_class_format == "HMI_inspection":
-    # class label and bins
-    # Depth
-    class_depth_label = ["0", "100", "200"] # Using lower bound as the label
-    class_depth_bins = [100, 200] # Only the middle bound values
-    print("class_depth_label = %s" % class_depth_label)
-    print("class_depth_bins = %s" % class_depth_bins)
-    # Roll
-    class_roll_nominal_value = np.array([-15, 0, 15]) # Note: the length of label should be one element longer than the bin
-    class_roll_label = [str(int(_e)) for _e in class_roll_nominal_value] # Using nominal value as class label
-    class_roll_bins = [-15, 15] # Only the middle bound values
-    print("class_roll_label = %s" % class_roll_label)
-    print("class_roll_bins = %s" % class_roll_bins)
-    # Pitch
-    class_pitch_nominal_value = np.array([-20, -10, 0, 10, 20]) # Note: the length of label should be one element longer than the bin
-    class_pitch_label = [str(int(_e)) for _e in class_pitch_nominal_value] # Using nominal value as class label
-    class_pitch_bins = [-20, -10, 10, 20] # Only the middle bound values
-    print("class_pitch_label = %s" % class_pitch_label)
-    print("class_pitch_bins = %s" % class_pitch_bins)
-    # Yaw
-    class_yaw_nominal_value = np.array([-20, -10, 0, 10, 20]) # Note: the length of label should be one element longer than the bin
-    class_yaw_label = [str(int(_e)) for _e in class_yaw_nominal_value] # Using nominal value as class label
-    class_yaw_bins = [-20, -10, 10, 20] # Only the middle bound values
-    print("class_yaw_label = %s" % class_yaw_label)
-    print("class_yaw_bins = %s" % class_yaw_bins)
-    #
-else:
-    pass
+# Get parameters for classification
+class_drpy_param_dict = TTBX.get_classification_parameters(drpy_class_format=drpy_class_format)
 #-------------------------------#
 
 
@@ -389,12 +344,12 @@ while (sample_count < DATA_COUNT) and (not received_SIGINT):
     # Classify ground truth data! (drpy class)
     #----------------------------------------------#
     _class_dict = dict()
-    _class_dict['distance'] = class_depth_label[ np.digitize( data_id_dict['distance'], class_depth_bins) ]
-    _class_dict['roll'] = class_roll_label[ np.digitize( data_id_dict['roll'], class_roll_bins) ]
-    _class_dict['pitch'] = class_pitch_label[ np.digitize( data_id_dict['pitch'], class_pitch_bins) ]
-    _class_dict['yaw'] = class_yaw_label[ np.digitize( data_id_dict['yaw'], class_yaw_bins) ]
-    #
+    _class_dict['distance'] = TTBX.classify_drpy(class_drpy_param_dict, data_id_dict['distance'], class_name="depth", is_classified_by_label=False)
+    _class_dict['roll']     = TTBX.classify_drpy(class_drpy_param_dict, data_id_dict['roll'], class_name="roll", is_classified_by_label=False)
+    _class_dict['pitch']    = TTBX.classify_drpy(class_drpy_param_dict, data_id_dict['pitch'], class_name="pitch", is_classified_by_label=False)
+    _class_dict['yaw']      = TTBX.classify_drpy(class_drpy_param_dict, data_id_dict['yaw'], class_name="yaw", is_classified_by_label=False)
     data_id_dict['class'] = _class_dict
+    #
     data_id_dict['file_name'] = "random_drpy_%s_%s_%s_%s" % (_class_dict['distance'], _class_dict['roll'], _class_dict['pitch'], _class_dict['yaw'])
     #----------------------------------------------#
 
