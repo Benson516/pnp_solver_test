e7e6ba1340c8586c09c8335655a5ca2feff9f7f7
benson516 <benson516@hotmail.com>
2022-08-30 12:28:35 +0800

    Add skeleton

---
+++ pnp_solver_2/face_pattern_solving_by_camera_angles_test.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas
+
+
+# Ground Truth (GT)
+#-----------------------------------#
+# GT face pattern in head frame
+
+
+# GT headpose
+
+
+# Generated GT face pattern in reference (ref.) frame
+
+
+# Randomly generate GT screen angles
+
+#-----------------------------------#
+
+# Measurement
+#-----------------------------------#
+# 1. Project the GT face pattern onto the image plane and add noise
+
+# 2. Add noise to GT screen angles as the measurements
+
+# Pack the measurements
+
+#-----------------------------------#
+
+
+# Solving the face pattern
+#-----------------------------------#
+# 1. Solver for face pattern in ref. frame
+
+# 2. Get headpose from keypoints
+
+# 3. TRansform to head local frame as the estimated golden pattern
+
+#-----------------------------------#
+
+
+# Error calculation
+#---------------------------------#
+
+#---------------------------------# 
